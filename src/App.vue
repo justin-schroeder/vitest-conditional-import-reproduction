e791df6e86a9e5d95a473968e2982a2da4cb21eb
Justin Schroeder <justin@formkit.com>
2024-01-15 18:13:13 -0500

    chore: app reflects test more closely

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+import { count } from 'vcir-package-a'
 import { CounterComponent } from 'vcir-package-b'
+count()
+count()
 </script>
 
 <template>
-  <CounterComponent />
-  <CounterComponent />
   <CounterComponent />
   <CounterComponent />
   <CounterComponent />
